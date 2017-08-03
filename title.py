fc9b33e6ab0c5f5276600c9a48f83c167827f578
Blayze95 <mjc13b@my.fsu.edu>
2017-08-03 16:02:14 -0400

    Add files via upload

--- title.py
+++ title.py
@@ -5,7 +5,7 @@ pygame.init()
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-GREY = (200, 200, 200)
+GREY = (30, 30, 30)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GOLD = (255, 255, 0)
@@ -53,6 +53,8 @@ class Title(object):
 					if event.key == pygame.K_z:
 						if self.sel_y == self.size_y/2:
 							display = False
+							
+							continue
 
 
 						if self.sel_y == self.size_y/2 + 80:
@@ -78,8 +80,63 @@ class Title(object):
 			pygame.quit()
 			return -1
 
+		self.newDisplay()
+
 		return 0
 
+	def newDisplay(self):
+		framecounter = 0
+		go = False
+		text1 = "This is a game inspired by Etrian Odyssey, made by Atlus."
+		text2 = "All of the ideas are recreations from those games."
+		text3 = "The maps are also pulled from those games."
+		text4 = "We hope you find it interesting."
+		text5 = "You are given 5 premade characters to navigate a maze."
+		text6 = "The goal is to get to the end of the maze,"
+		text7 = "and defeat the boss."
+
+		small = pygame.font.Font(None, 36)
+		t1 = small.render(text1, 1, WHITE)
+		t2 = small.render(text2, 1, WHITE)
+		t3 = small.render(text3, 1, WHITE)
+		t4 = small.render(text4, 1, WHITE)
+		t5 = small.render(text5, 1, WHITE)
+		t6 = small.render(text6, 1, WHITE)
+		t7 = small.render(text7, 1, WHITE)
+
+		while framecounter <= 500:
+
+			self.screen.fill(BLACK)
+			pygame.draw.rect(self.screen, WHITE, [self.size_x/16, self.size_y/16, self.size_x*7/8, self.size_y*7/8])
+			pygame.draw.rect(self.screen, GREY, [self.size_x/16+2, self.size_y/16+2, self.size_x*7/8-4, self.size_y*7/8-4])
+
+			if framecounter > 60:
+				self.screen.blit(t1, (self.size_x/16+10, self.size_y/16+20))
+			if framecounter > 120:
+				self.screen.blit(t2, (self.size_x/16+10, self.size_y/16+90))
+
+			if framecounter > 180:
+				self.screen.blit(t3, (self.size_x/16+10, self.size_y/16+170))
+
+			if framecounter > 240:
+				self.screen.blit(t5, (self.size_x/16+10, self.size_y/16+250))
+
+			if framecounter > 300:
+				self.screen.blit(t6, (self.size_x/16+10, self.size_y/16+330))
+
+			if framecounter > 360:
+				self.screen.blit(t7, (self.size_x/16+10, self.size_y/16+410))
+
+			if framecounter > 420:
+				self.screen.blit(t4, (self.size_x/16+10, self.size_y/16+490))
+
+			#if framecounter > 180:
+
+			#if framecounter > 240:
+
+			framecounter += 1
+			clock.tick(60)
+			pygame.display.flip()
 
 
 if __name__ == "__main__":
